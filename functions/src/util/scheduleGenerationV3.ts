8934bcb7784b1bb2df11973e69a5cbef68c0c691
Trịnh Minh Anh <48356852+peterburgs@users.noreply.github.com>
2021-08-18 23:00:30 +0700

    encrypt scheduling algorithm

--- functions/src/util/scheduleGenerationV3.ts
+++ functions/src/util/scheduleGenerationV3.ts
@@ -8,79 +8,62 @@ import Semester from "../models/Semester";
 import { ILab, ITeaching } from "../types";
 
 const scheduleGenerationV3 = async (
-  oldLabs: ILab[],
-  newLabs: ILab[],
-  teachings: ITeaching[],
-  semesterId: string,
-  numberOfWeeks: number,
-  numberOfPeriods: number
+  ol: ILab[],
+  nl: ILab[],
+  ts: ITeaching[],
+  si: string,
+  nows: number,
+  nops: number
 ) => {
   const session = await mongoose.startSession();
   try {
     await session.withTransaction(async () => {
-      let semester = await Semester.findById({
-        _id: semesterId,
+      let smt = await Semester.findById({
+        _id: si,
         isHidden: false,
       });
-      let teachingQueue: ITeaching[] = [];
-      teachings.forEach((t) => teachingQueue.push(t));
-      teachingQueue.sort((a, b) => b.numberOfStudents - a.numberOfStudents);
-      let labQueue: ILab[] = [];
-      oldLabs.forEach((l) => labQueue.push(l));
-      labQueue.sort((a, b) => b.capacity - a.capacity);
-      newLabs.sort((a, b) => moment(a.createdAt!).diff(moment(b.createdAt)));
-      newLabs.forEach((l) => labQueue.push(l));
-      let { labSchedule } = semester!;
+      let tq: ITeaching[] = [];
+      ts.forEach((t) => tq.push(t));
+      tq.sort((a, b) => b.numberOfStudents - a.numberOfStudents);
+      let lq: ILab[] = [];
+      ol.forEach((l) => lq.push(l));
+      lq.sort((a, b) => b.capacity - a.capacity);
+      nl.sort((a, b) => moment(a.createdAt!).diff(moment(b.createdAt)));
+      nl.forEach((l) => lq.push(l));
+      let { labSchedule } = smt!;
       if (!labSchedule || labSchedule.length <= 0) {
-        labSchedule = Array(labQueue.length * numberOfPeriods + 1)
+        labSchedule = Array(lq.length * nops + 1)
           .fill(0)
-          .map(() => Array(numberOfWeeks * 7).fill(0));
+          .map(() => Array(nows * 7).fill(0));
       }
-
-      while (teachingQueue.length) {
-        let currentTeaching = teachingQueue.shift()!;
-        let availableDaysForCurrentTeaching: {
+      while (tq.length) {
+        let ct = tq.shift()!;
+        let adfct: {
           weekNo: number;
           dayOfWeek: number;
           lab: string;
           currentDay: number;
         }[] = [];
-        for (let i = 0; i < labQueue.length; i++) {
-          for (
-            let j = semester!.startPracticalWeek * 7;
-            j < numberOfWeeks * 7;
-            j++
-          ) {
-            let currentWeek = Math.floor(j / 7);
-            if (currentWeek < currentTeaching.startPracticalWeek) break;
-            let currentDayOfWeek = j % 7;
-
-            if (currentDayOfWeek == currentTeaching?.dayOfWeek) {
-              if (
-                availableDaysForCurrentTeaching.filter(
-                  (w) => w.weekNo == currentWeek
-                ).length == 0
-              ) {
-                if (
-                  availableDaysForCurrentTeaching.filter(
-                    (w) => w.weekNo + 1 == currentWeek
-                  ).length == 0
-                ) {
-                  if (
-                    currentTeaching.numberOfPracticalWeeks >
-                    availableDaysForCurrentTeaching.length
-                  ) {
+        for (let i = 0; i < lq.length; i++) {
+          for (let j = smt!.startPracticalWeek * 7; j < nows * 7; j++) {
+            let cw = Math.floor(j / 7);
+            if (cw < ct.startPracticalWeek) break;
+            let cdow = j % 7;
+            if (cdow == ct?.dayOfWeek) {
+              if (adfct.filter((w) => w.weekNo == cw).length == 0) {
+                if (adfct.filter((w) => w.weekNo + 1 == cw).length == 0) {
+                  if (ct.numberOfPracticalWeeks > adfct.length) {
                     for (
-                      let index = currentTeaching.startPeriod;
-                      index <= currentTeaching.endPeriod;
+                      let index = ct.startPeriod;
+                      index <= ct.endPeriod;
                       index++
                     ) {
                       if (labSchedule[index + 15 * i][j] === 1) break;
-                      if (index == currentTeaching.endPeriod) {
-                        availableDaysForCurrentTeaching.push({
-                          weekNo: currentWeek,
-                          dayOfWeek: currentDayOfWeek,
-                          lab: labQueue[i]._id,
+                      if (index == ct.endPeriod) {
+                        adfct.push({
+                          weekNo: cw,
+                          dayOfWeek: cdow,
+                          lab: lq[i]._id,
                           currentDay: j,
                         });
                       }
@@ -92,24 +75,20 @@ const scheduleGenerationV3 = async (
           }
         }
 
-        for (let item of availableDaysForCurrentTeaching) {
+        for (let item of adfct) {
           let labUsage = new LabUsage({
-            semester: semesterId,
+            semester: si,
             lab: item.lab,
-            teaching: currentTeaching._id,
+            teaching: ct._id,
             weekNo: item.weekNo,
             dayOfWeek: item.dayOfWeek,
-            startPeriod: currentTeaching.startPeriod,
-            endPeriod: currentTeaching.endPeriod,
+            startPeriod: ct.startPeriod,
+            endPeriod: ct.endPeriod,
             isHidden: false,
           });
-          for (
-            let index = currentTeaching.startPeriod;
-            index <= currentTeaching.endPeriod;
-            index++
-          ) {
+          for (let index = ct.startPeriod; index <= ct.endPeriod; index++) {
             labSchedule[
-              index + 15 * labQueue.findIndex((lab) => lab._id == item.lab)
+              index + 15 * lq.findIndex((lab) => lab._id == item.lab)
             ][item.currentDay] = 1;
           }
           labUsage = await labUsage.save({ session });
@@ -120,7 +99,7 @@ const scheduleGenerationV3 = async (
       }
       await Semester.findByIdAndUpdate(
         {
-          _id: semester?._id,
+          _id: smt?._id,
           isHidden: false,
         },
         {
